b4af1171e167b8cc3b90e2e512ac0823aafbd1a1
abhishek-kumar98 <abhishekkumar.best@gmail.com>
2019-06-24 13:58:07 +0530

    Signed-off-by: abhishek-kumar98 <abhishekkumar.best@gmail.com>

--- af_project_courseweb_frontend/src/App.js
+++ af_project_courseweb_frontend/src/App.js
@@ -5,6 +5,8 @@ import RegisterComponent from "./Components/Register_Component" ;
 import HomeComponent from "./Components/Home_Component" ;
 import StudentProfile from "./Components/Student_Profile_Component" ;
 import Admin_profile from "./Components/Admin/Admin_profile_component";
+import Instructor_profile from "./Components/Instructor/Instructor_Profile_Component";
+
 
 import { Layout } from './styles/Layout';
 import { NavigationBar } from './styles/NavigationBar';
@@ -24,6 +26,7 @@ class App extends Component {
                 <Route path="/login" component={LoginComponent}/>
                 <Route path="/student_profile" component={StudentProfile}/>
                 <Route path="/admin_profile" component={Admin_profile}/>
+                <Route path="/instructor_profile" component={Instructor_profile}/>
               </Switch>
             </Layout>
           </Router>
